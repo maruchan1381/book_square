5882d591572e7e4b7c372575c91659e23d19bffa
maruchan1381 <haru0328mh@gmail.com>
2021-01-02 16:49:01 +0900

    環境変数設定

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-    c.application_id = '1039117070079090412'
-    c.affiliate_id = '1e246a5d.03911d62.1e246a5e.a232c776'
+    c.application_id = ENV["RWS_APPLICATION_ID"]    
+    c.affiliate_id = ENV["RWS_AFFILIATION_ID"]
 end
