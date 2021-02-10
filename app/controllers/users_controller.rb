1d23a203d1c8a345da615851d334dcf40bbf273f
maruchan1381 <haru0328mh@gmail.com>
2021-02-10 13:28:25 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = current_user.nickname
-    @books = current_user.books
+    @books = current_user.books.includes(:review)
   end
 end
